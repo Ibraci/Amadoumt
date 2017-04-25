cfbe620f444cbc6af36cde57e1c8838ee710e8ad
Ibrahim CISSE <ibraci1960@gmail.com>
2017-04-25 15:31:28 +0000

    Ajout du formulaire de add_news.php

--- admin/views/add_news.view.php
+++ admin/views/add_news.view.php
@@ -2,4 +2,24 @@
 	$title = "Ajouter un article";
 	include('partials/_header.php'); ?>
 
+	<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					Ajouter un article au blog
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-9 col-md-offset-1">
+							<input type="text" name="titre" class="form-control" placeholder="Titre">
+							<textarea name="chapo" rows="3" class="form-control" placeholder="Chapo de l'article"></textarea>
+							<textarea name="contenu" rows="3" class="form-control" placeholder="Contenu de l'article"></textarea>
+							<button type="submit" class="btn btn-primary">Envoyer</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 <?php include('partials/_footer.php'); ?>
